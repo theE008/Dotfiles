04bfa9e5f85d385851d1a675b84392ee560d6dbd
Thiago Pereira de Oliveira <144809113+theE008@users.noreply.github.com>
2026-05-05 07:34:16 -0300

    needed

--- config/reasons.bash
+++ config/reasons.bash
@@ -1,42 +1,42 @@
 ### UI
-sudo pacman -S tmux --noconfirm # Reason: Multiplexador de terminal
-sudo pacman -S foot --noconfirm # Reason: Terminal para sway
-sudo pacman -S sway swaybg swayidle swaylock waybar wayland-protocols wl-clipboard xorg-xwayland --noconfirm # Reason: Pacotes necessários para sway
-sudo pacman -S brightnessctl --noconfirm # Reason: Controle do brilho
-sudo pacman -S ttf-terminus-nerd --noconfirm # Reason: Fonte bonita
-sudo pacman -S bemenu --noconfirm # Reason: Testando outro app launcher
-sudo pacman -S slurp grim --noconfirm # Reason: Capturas de tela
-sudo pacman -S less --noconfirm # Reason: Essencial para o gitree
-sudo pacman -S nodejs npm python python-pip --noconfirm # Reason: Ferramentas para o neovim
-sudo pacman -S bluez bluez-utils --noconfirm # Reason: Bluetooth
-sudo pacman -S pipewire pipewire-pulse pipewire-alsa wireplumber libpulse alsa-plugins --noconfirm # Reason: Audio Stack
-sudo pacman -S xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk --noconfirm # Reason: Interface de popup
-yay -S swaylock-effects --noconfirm # Reason: Screen Lock
-sudo pacman -S openssh --noconfirm # Reason: SSH
+sudo pacman -S tmux --needed --noconfirm # Reason: Multiplexador de terminal
+sudo pacman -S foot --needed --noconfirm # Reason: Terminal para sway
+sudo pacman -S sway swaybg swayidle swaylock waybar wayland-protocols wl-clipboard xorg-xwayland --needed --noconfirm # Reason: Pacotes necessários para sway
+sudo pacman -S brightnessctl --needed --noconfirm # Reason: Controle do brilho
+sudo pacman -S ttf-terminus-nerd --needed --noconfirm # Reason: Fonte bonita
+sudo pacman -S bemenu --needed --noconfirm # Reason: Testando outro app launcher
+sudo pacman -S slurp grim --needed --noconfirm # Reason: Capturas de tela
+sudo pacman -S less --needed --noconfirm # Reason: Essencial para o gitree
+sudo pacman -S nodejs npm python python-pip --needed --noconfirm # Reason: Ferramentas para o neovim
+sudo pacman -S bluez bluez-utils --needed --noconfirm # Reason: Bluetooth
+sudo pacman -S pipewire pipewire-pulse pipewire-alsa wireplumber libpulse alsa-plugins --needed --noconfirm # Reason: Audio Stack
+sudo pacman -S xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk --needed --noconfirm # Reason: Interface de popup
+yay -S swaylock-effects --needed --noconfirm # Reason: Screen Lock
+sudo pacman -S openssh --needed --noconfirm # Reason: SSH
 
 ### UX
-sudo pacman -S btop --noconfirm # Reason: Visualizador de eficiência do sistema
-sudo pacman -S fastfetch --noconfirm # Reason: Visualizador de OS
-sudo pacman -S kew --noconfirm # Reason: Music player
-sudo pacman -S lsd --noconfirm # Reason: Riced cd
-sudo pacman -S mpv --noconfirm # Reason: Player de mídia
-sudo pacman -S w3m --noconfirm # Reason: Browser de CLI
-sudo pacman -S mandoc arch-wiki-lite --noconfirm # Reason: Toda a wiki que preciso
-sudo pacman -S fzf --noconfirm # Reason: Fuzzy Search
-sudo pacman -S yt-dlp --noconfirm # Reason: w3m youtube watcher
-sudo pacman -S tree-sitter --noconfirm # Reason: NVim dependency
-sudo pacman -S tree-sitter-cli --noconfirm # Reason: NVim dependency
-sudo pacman -S python-pylatexenc --noconfirm # Reason: Nvim dependency
+sudo pacman -S btop --needed --noconfirm # Reason: Visualizador de eficiência do sistema
+sudo pacman -S fastfetch --needed --noconfirm # Reason: Visualizador de OS
+sudo pacman -S kew --needed --noconfirm # Reason: Music player
+sudo pacman -S lsd --needed --noconfirm # Reason: Riced cd
+sudo pacman -S mpv --needed --noconfirm # Reason: Player de mídia
+sudo pacman -S w3m --needed --noconfirm # Reason: Browser de CLI
+sudo pacman -S mandoc arch-wiki-lite --needed --noconfirm # Reason: Toda a wiki que preciso
+sudo pacman -S fzf --needed --noconfirm # Reason: Fuzzy Search
+sudo pacman -S yt-dlp --needed --noconfirm # Reason: w3m youtube watcher
+sudo pacman -S tree-sitter --needed --noconfirm # Reason: NVim dependency
+sudo pacman -S tree-sitter-cli --needed --noconfirm # Reason: NVim dependency
+sudo pacman -S python-pylatexenc --needed --noconfirm # Reason: Nvim dependency
 
 ### INTERNET
-sudo pacman -S networkmanager --noconfirm # Reason: Melhor sistema de conexão a internet
-sudo pacman -S flatpak --noconfirm # Reason: Instalador de pacotes mais complexos
-sudo pacman -S syncthing --noconfirm # Reason: Sincronização de arquivos com celular
-sudo pacman -S ufw --noconfirm # Reason: Firewall
-sudo pacman -S steam --noconfirm # Reason: Baixar Jogos
-yay -S librewolf-bin --noconfirm # Reason: Browser Seguro
+sudo pacman -S networkmanager --needed --noconfirm # Reason: Melhor sistema de conexão a internet
+sudo pacman -S flatpak --needed --noconfirm # Reason: Instalador de pacotes mais complexos
+sudo pacman -S syncthing --needed --noconfirm # Reason: Sincronização de arquivos com celular
+sudo pacman -S ufw --needed --noconfirm # Reason: Firewall
+sudo pacman -S steam --needed --noconfirm # Reason: Baixar Jogos
+yay -S librewolf-bin --needed --noconfirm # Reason: Browser Seguro
 
 ### MANIPULAÇÃO
-sudo pacman -S neovim  --noconfirm # Reason: Editor de texto
-sudo pacman -S zip unzip  --noconfirm # Reason: Compressão de arquivos
-sudo pacman -S python-pillow  --noconfirm # Reason: Para brand
+sudo pacman -S neovim --needed --noconfirm # Reason: Editor de texto
+sudo pacman -S zip unzip --needed --noconfirm # Reason: Compressão de arquivos
+sudo pacman -S python-pillow --needed --noconfirm # Reason: Para brand
